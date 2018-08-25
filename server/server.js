aa5d14f4e56f66ef00a114dd5062ec30f042b234
Manohar Kurapati <kvmanohar@gmail.com>
2018-08-25 16:01:07 +0100

    Added GET Endpoint to get all the todos

--- server/server.js
+++ server/server.js
@@ -26,6 +26,18 @@ app.post("/todos", (req, res) => {
 	);
 });
 
+//GET Endpoint
+app.get("/todos", (req, res) => {
+	Todo.find().then(
+		todos => {
+			res.send({ todos });
+		},
+		e => {
+			res.status(400).send(e);
+		}
+	);
+});
+
 app.listen(3000, () => {
 	console.log("Started on port 3000");
 });
