dbf03a3df61d9b684f8d5a388c81756d9c260a7c
Manohar Kurapati <kvmanohar@gmail.com>
2018-08-26 22:06:03 +0100

    print env variables for debugging

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,8 @@ var mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
 // mongoose.connect("mongodb://localhost:27017/TodoApp");
+console.log(process.env.MONGODB_URI);
+
 mongoose.connect(
 	process.env.MONGODB_URI || "mongodb://localhost:27017/TodoApp"
 );
--- server/server.js
+++ server/server.js
@@ -7,6 +7,8 @@ var { mongoose } = require("./db/mongoose");
 var { Todo } = require("./models/todo");
 var { User } = require("./models/user");
 
+console.log(process.env.port);
+
 const port = process.env.port || 3000;
 
 var app = express();
