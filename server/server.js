bb9893bd9c777220eb14b03f732bca3dd343bd79
Manohar Kurapati <kvmanohar@gmail.com>
2018-08-26 12:31:58 +0100

    Add new GET endpoint tods/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var { ObjectID } = require("mongodb");
 
 //Local imports
 var { mongoose } = require("./db/mongoose");
@@ -11,7 +12,7 @@ var app = express();
 //Middleware to convert the app req to JSON
 app.use(bodyParser.json());
 
-//POST Funciton
+//POST Endpoint: /todos
 app.post("/todos", (req, res) => {
 	var todo = new Todo({
 		text: req.body.text
@@ -26,7 +27,7 @@ app.post("/todos", (req, res) => {
 	);
 });
 
-//GET Endpoint
+//GET Endpoint: /todos
 app.get("/todos", (req, res) => {
 	Todo.find().then(
 		todos => {
@@ -36,6 +37,25 @@ app.get("/todos", (req, res) => {
 			res.status(400).send(e);
 		}
 	);
+});
+
+//GET Endpoint: /todos/1243452 ---> Get todo item by passing id
+app.get("/todo/:id", (req, res) => {
+	var id = req.params.id;
+	if (!ObjectID.isValid(id)) {
+		return res.status(404).send();
+	}
+
+	Todo.findById(id)
+		.then(todo => {
+			if (!todo) {
+				return res.status(404).send(null);
+			}
+			res.status(200).send({ todo });
+		})
+		.catch(e => {
+			res.status(400).send();
+		});
 });
 
 app.listen(3000, () => {
