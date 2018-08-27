d23014b21605cbc3a617a99627d32346f741cadc
Manohar Kurapati <kvmanohar@gmail.com>
2018-08-27 09:00:59 +0100

    DELETE todo endpoint added

--- server/server.js
+++ server/server.js
@@ -60,6 +60,23 @@ app.get("/todo/:id", (req, res) => {
 		});
 });
 
+app.delete("/todo/:id", (req, res) => {
+	var id = req.params.id;
+	if (!ObjectID.isValid(id)) {
+		return res.status(404).send();
+	}
+	Todo.findOneAndDelete({ _id: id })
+		.then(todo => {
+			if (!todo) {
+				return res.status(404).send();
+			}
+			res.status(200).send(todo);
+		})
+		.catch(e => {
+			res.status(400).send(e);
+		});
+});
+
 app.listen(port, () => {
 	console.log(`Started on port ${port}`);
 });
