ef04a280f180f17f0c4e398eb3ab9f11ea114328
Manohar Kurapati <kvmanohar@gmail.com>
2018-09-01 18:58:45 +0100

    created a config.js to set up test env and test database

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,9 +4,7 @@ mongoose.Promise = global.Promise;
 // mongoose.connect("mongodb://localhost:27017/TodoApp");
 console.log(`MongoDB Heroku URI: ${process.env.MONGODB_URI}`);
 
-mongoose.connect(
-	process.env.MONGODB_URI || "mongodb://localhost:27017/TodoApp"
-);
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {
 	mongoose
