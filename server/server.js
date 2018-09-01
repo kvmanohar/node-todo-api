77fdf5d1a20a0b751a96715d78fb86bec98a64fb
Manohar Kurapati <kvmanohar@gmail.com>
2018-09-01 07:50:41 +0100

    Added PATCH endpoint to update todo

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var { ObjectID } = require("mongodb");
+const _ = require("lodash");
+const express = require("express");
+const bodyParser = require("body-parser");
+const { ObjectID } = require("mongodb");
 
 //Local imports
 var { mongoose } = require("./db/mongoose");
@@ -75,6 +76,34 @@ app.delete("/todo/:id", (req, res) => {
 		})
 		.catch(e => {
 			res.status(400).send(e);
+		});
+});
+
+//Update Endpoint: /todo/:id ---> Update todo item by passing todo
+app.patch("/todo/:id", (req, res) => {
+	var id = req.params.id;
+	var body = _.pick(req.body, ["text", "completed"]);
+
+	if (!ObjectID.isValid(id)) {
+		return res.status(404).send();
+	}
+
+	if (_.isBoolean(body.completed) && body.completed) {
+		body.completedAt = new Date().getTime();
+	} else {
+		body.completed = false;
+		body.completedAt = null;
+	}
+
+	Todo.findByIdAndUpdate(id, { $set: body }, { new: true })
+		.then(todo => {
+			if (!todo) {
+				return res.status(404).send();
+			}
+			res.status(200).send({ todo });
+		})
+		.catch(e => {
+			res.status(400).send();
 		});
 });
 
